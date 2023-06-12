2299abf7607c67d7b8360f2803de01173dd4c463
Edouard Koehn <edouard.koehn@eplf.ch>
2023-06-12 16:00:52 +0000

    last commit

--- clustering/similarity_boostrap.py
+++ clustering/similarity_boostrap.py
@@ -25,7 +25,18 @@ annotation = pd.read_csv(PATH_ANNOTATION)
 annotation = annotation.sort_values(["n_cluster", "batch"]).reset_index(drop=True)
 
 # Extract the path information
-experiments = [4, 5, 8, 10, 12, 14, 20, 30]
+experiments = [6]  # to modify depending on the experiments
+experiments = [
+    4,
+    6,
+    8,
+    10,
+    12,
+    14,
+    16,
+    18,
+    20,
+]  # to modify depending on the experiments
 logging.info(f"Number of experiments: {len(experiments)}")
 logging.info(f"Experiment values: {experiments}")
 cluster_paths = []
@@ -35,26 +46,26 @@ for exp in experiments:
     )
     logging.info(f"Number of batch-{exp}: {len(cluster_paths[-1])}")
 
-# Compute the MNI distance
-distance_means = []
-distance_std = []
-for exp in range(len(experiments)):
-    logging.info(f"Similarity MNI exp: {experiments[exp]}")
-    distance_matrix = compute.compute_simlarity(cluster_paths[exp], "MNI")
-    distance_means.append(np.mean(distance_matrix, axis=0))
-    logging.info(f"Mean: {distance_means[-1]}")
-    distance_std.append(np.std(distance_matrix, axis=0))
-    logging.info(f"Std: {distance_std[-1]}")
+# # Compute the MNI distance
+# distance_means = []
+# distance_std = []
+# for exp in range(len(experiments)):
+#     logging.info(f"Similarity MNI exp: {experiments[exp]}")
+#     distance_matrix = compute.compute_simlarity(cluster_paths[exp], "MNI")
+#     distance_means.append(np.mean(distance_matrix, axis=0))
+#     logging.info(f"Mean: {distance_means[-1]}")
+#     distance_std.append(np.std(distance_matrix, axis=0))
+#     logging.info(f"Std: {distance_std[-1]}")
 
-# Export the mni similarity
-df_distance_mni = pd.DataFrame.from_dict(
-    {
-        "experiement": experiments,
-        "mean_similarity": distance_means,
-        "std_similarity": distance_std,
-    }
-)
-df_distance_mni.to_csv(PATH_SIM_MNI, index=False)
+# # Export the mni similarity
+# df_distance_mni = pd.DataFrame.from_dict(
+#     {
+#         "experiement": experiments,
+#         "mean_similarity": distance_means,
+#         "std_similarity": distance_std,
+#     }
+# )
+# df_distance_mni.to_csv(PATH_SIM_MNI, index=False)
 
 # Compute the element centric distance
 distance_means = []
